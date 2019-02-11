61b42a16ad2198773680a62b3c23b5f9517a02d8
2D1J <dedecker.joshua@gmail.com>
2019-02-11 14:03:11 +0100

    modifs

--- ex_5/script.js
+++ ex_5/script.js
@@ -1,3 +1,5 @@
-a = parseInt(prompt("Indiquez un nombre à virgule :"));
-b = prompt("Indiquez un second nombre à virgule :");
-alert("Egal = "+ a * b );
+function validation() {
+    a = document.getElementById("premier_nombre").value;
+    b = document.getElementById("deuxieme_nombre").value;
+    alert("Egal = "+ parseInt(a) * b );
+}
