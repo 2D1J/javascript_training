d242452e6340feb1b460123092a1e7ac06ef6a6e
2D1J <dedecker.joshua@gmail.com>
2019-02-11 14:56:03 +0100

    fin première série

--- ex_6/script.js
+++ ex_6/script.js
@@ -1,4 +1,5 @@
-a = parseInt(prompt("Indiquez un nombre à virgule :"));
-b = prompt("Indiquez un second nombre à virgule :");
-c = a*b;
-alert("Egal = "+ c );
+function validation() {
+    a = document.getElementById("premier_nombre").value;
+    b = document.getElementById("deuxieme_nombre").value;
+    alert("Modulus! = "+ a%b );
+}
