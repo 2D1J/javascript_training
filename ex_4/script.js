a6a54832e25be7841b7d93feb1d0fd74d5d9828b
2D1J <dedecker.joshua@gmail.com>
2019-02-11 13:49:20 +0100

    modifs

--- ex_4/script.js
+++ ex_4/script.js
@@ -1,5 +1,6 @@
-var prenom = prompt("Indiquez votre prénom.")
-var nom = prompt("Indiquez votre nom.")
-var ville = prompt("Indiquez votre ville.")
-
-alert("Vous êtes " + nom + " " + prenom + " et votre ville est " + ville)
+function validation() {
+    nom = document.getElementById("nom").value;
+    prenom = document.getElementById("prenom").value;
+    ville = document.getElementById("ville").value;
+    alert("Nom : " + nom + "\n" + "Prenom : " + prenom + "\n" + "Ville : " + ville);
+}
