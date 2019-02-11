ddb23c3d3da4946147973987fa9de85e798dd9da
2D1J <dedecker.joshua@gmail.com>
2019-02-11 16:07:04 +0100

    deuxième exo série 2

--- Serie_2/script.js
+++ Serie_2/script.js
@@ -11,3 +11,11 @@ function periRectangle() {
     resultat = 2*(longueur+largeur);
     document.getElementById("perimetreCalc").innerHTML = resultat;
 }
+
+function surfaceCercle() {
+    rayon = Number(document.getElementById("rayon").value);
+    r2 = rayon*rayon;
+    console.log(r2);
+    resultat = 3.16*r2;
+    document.getElementById("surfaceCalc_cercle").innerHTML = resultat;
+}
