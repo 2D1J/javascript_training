13b22ce3b5071a023f943642613591a4bcc34f68
2D1J <dedecker.joshua@gmail.com>
2019-02-19 14:51:13 +0100

    Jasmine terminé

--- Serie_6/questions.js
+++ Serie_6/questions.js
@@ -9,18 +9,18 @@ var CreationTableauNombres = function () {
 }
 
 var RemplacementElement = function (langages) {
-    langages[2] = 'Javascript';
-    console.log(langages);
-    langages;
+    langages[2] = "Javascript";
+    return langages;
 }
 
 var AjoutElementLangages = function (langages) {
-    langages = langages.push('Ruby', 'Python');
+    langages.push('Ruby', 'Python');
     return langages;
 }
 
 var AjoutElementNombres = function (nombres) {
-
+    nombres.unshift(-2, -1);
+    return nombres;
 }
 
 var SuppressionPremierElement = function (langages) {
@@ -30,22 +30,28 @@ var SuppressionPremierElement = function (langages) {
 
 var SuppressionDernierElement = function (langages) {
     langages.pop();
+    return langages;
 }
 
 var ConversionChaineTableau = function (reseaux_sociaux_chaine) {
-    console.log(reseaux_sociaux_chaine);
-    reseaux_sociaux_chaine = reseaux_sociaux;
-    console.log(reseaux_sociaux);
+    console.log("reseaux_sociaux_chaine = " + reseaux_sociaux_chaine);
+    var reseaux_sociaux = reseaux_sociaux_chaine.split(',');
+    console.log("reseaux_sociaux_chaine (après split) = " +reseaux_sociaux_chaine);
+    console.log("array = " + reseaux_sociaux);
+    return reseaux_sociaux;
 }
 
 var ConversionTableauChaine = function (langages) {
-
+    langages_chaine = langages.toString();
+    return langages_chaine;
 }
 
 var TriTableau = function (reseaux_sociaux) {
-
+    reseaux_sociaux.sort();
+    return reseaux_sociaux;
 }
 
 var InversionTableau = function (reseaux_sociaux){
-
+    reseaux_sociaux.reverse();
+    return reseaux_sociaux;
 }
